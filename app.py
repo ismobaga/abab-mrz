5fff637ba8af0787b0482b07c37bda0015f98f63
Ismail Bagayoko <bagayoko.ismail@gmail.com>
2020-05-27 05:21:43 -0300

    ??

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, Flask, redirect, jsonify, render_template
+from flask import request, Flask, redirect, jsonify, render_template, json
 from time import time
 import os
 import io
@@ -14,49 +14,61 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def file_ext(filename):
-  return filename.split('.', 1)[-1].lower()
-def allowed_file(filename):
-  return file_ext(filename)  in ALLOWED_EXTENSIONS
 
+def file_ext(filename):
+    return filename.split('.', 1)[-1].lower()
+
+
+def allowed_file(filename):
+    return file_ext(filename) in ALLOWED_EXTENSIONS
 
 
 @app.route('/mrzdata', methods=['POST'])
 def mrzdata():
-  if request.files.get("file"):
-    file = request.files['file']
-    if file and allowed_file(file.filename):  
-      filename = str(time()).split(".")[0] + file_ext(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      imgpath = os.path.dirname(os.path.realpath(__file__))+"/uploads/"+filename
-      img  = getMRZFromImg(imgpath=imgpath)
-      text = mrzToText(img)
-      data, t  = getMRZData(text)
-      return jsonify( {
-        "mrzdata": data,
-    })
+    if request.files.get("file"):
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = str(time()).split(".")[0] + file_ext(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            imgpath = os.path.dirname(
+                os.path.realpath(__file__))+"/uploads/"+filename
+            img = getMRZFromImg(imgpath=imgpath)
+            text = mrzToText(img)
+            data, t = getMRZData(text)
+            response = app.response_class(
+                response=json.dumps({
+                    "mrzdata": data,
+                }),
+                status=200,
+                mimetype='application/json'
+            )
+            return response
+    return {
+      'error ': 'File missing'
+    }
 
 
 @app.route('/', methods=['POST'])
 def upload_file():
-  if request.files.get("file"):
-    file = request.files['file']
-    if file and allowed_file(file.filename):  
-      filename = str(time()).split(".")[0] + file_ext(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      imgpath = os.path.dirname(os.path.realpath(__file__))+"/uploads/"+filename
-      img  = getMRZFromImg(imgpath=imgpath)
-      text = mrzToText(img)
-      data, t  = getMRZData(text)
+    if request.files.get("file"):
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = str(time()).split(".")[0] + file_ext(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            imgpath = os.path.dirname(
+                os.path.realpath(__file__))+"/uploads/"+filename
+            img = getMRZFromImg(imgpath=imgpath)
+            text = mrzToText(img)
+            data, t = getMRZData(text)
 
-      return render_template('result.html', resultat=data)
-  return render_template('index.html')
-        
+            return render_template('result.html', resultat=data)
+    return render_template('index.html')
+
 
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html', resultat=None)
+    return render_template('index.html', resultat=None)
 
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
